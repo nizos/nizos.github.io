92ab942e14f6af0aefb5fcf7676a2d325229e450
Nizar Selander <nizar.selander@gmail.com>
2023-02-27 14:20:49 +0100

    test docs release

--- .eleventy.js
+++ .eleventy.js
@@ -46,7 +46,7 @@ module.exports = function(eleventyConfig) {
     return {
         dir: {
             input: 'src',
-            output: 'public',
+            output: 'docs',
             includes: 'includes',
             data: 'data',
             layouts: 'layouts'
