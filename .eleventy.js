bb975e0b42103751b9435366b720be86a12007ea
Nizar Selander <nizar.selander@gmail.com>
2023-02-28 08:46:25 +0100

    feat: added cname for github pages and changed output dir
    Changed the output directory to docs to make it easier to deploy to github pages

--- .eleventy.js
+++ .eleventy.js
@@ -43,10 +43,12 @@ module.exports = function(eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy("src/css");
 
+    eleventyConfig.addPassthroughCopy("src/CNAME");
+
     return {
         dir: {
             input: 'src',
-            output: 'public',
+            output: 'docs',
             includes: 'includes',
             data: 'data',
             layouts: 'layouts'
