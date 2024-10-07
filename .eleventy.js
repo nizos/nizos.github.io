aa1c2d6c4ba4112a93af04f2af7a2830037ac6b0
Nizar Selander <nizar.selander@factor10.com>
2024-10-07 10:57:53 +0200

    ci: added deployment workflow

--- .eleventy.js
+++ .eleventy.js
@@ -65,7 +65,7 @@ module.exports = function(eleventyConfig) {
     return {
         dir: {
             input: 'src',
-            output: 'docs',
+            output: '_site',
             includes: 'includes',
             data: 'data',
             layouts: 'layouts'
